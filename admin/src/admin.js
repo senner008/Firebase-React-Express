9e9aaa55efcadc33ec3693a42fbb922653dddf1e
nig <nig@beepanalytics.com>
2020-03-02 00:31:25 +0100

    wip

--- admin/src/admin.js
+++ admin/src/admin.js
@@ -1,11 +1,18 @@
 
-const serviceAccount = require("../config/private-admin-key.json");
+
+if (process.env.NODE_ENV === "production") {
+
+}
+else {
+    var serviceAccount = require("../config/private-admin-key.json");
+}
+
 const admin = require("firebase-admin");
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: "https://node-express-auth.firebaseio.com"
 });
-
+console.log(process.env.NODE_ENV)
 module.exports = admin;
   
