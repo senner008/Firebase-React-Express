486814c9458558d1d2cfd3dfadb284e24efd7e4e
nig <nig@beepanalytics.com>
2020-03-07 13:47:44 +0100

    readme update

--- admin/src/app.js
+++ admin/src/app.js
@@ -22,8 +22,6 @@ app.post('/auth', async (req, res) => {
     token =  req.headers.authorization.split(' ')[1];
   }
   try {
-    console.log(token)
-    
     await admin.auth().verifyIdToken(token);
     res.json({body : "Main content"});
   }
