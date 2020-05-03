6e4588faaab58cf8946c200e78dcfa2cdd8c2f28
nig <nig@beepanalytics.com>
2020-05-03 20:16:31 +0200

    get name from azure

--- admin/src/app.js
+++ admin/src/app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const path = require('path');
 const logger = require('morgan');
 const app = express();
-const cors = require('cors')
+const cors = require('cors');
+const fetch = require('node-fetch');
 const port = process.env.PORT || 5000;
 
 app.use(logger('dev'));
@@ -16,6 +17,15 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.post('/auth', checkIfAdmin, async (_, res) => {
   return res.send({body : "Main content"});
 });
+
+app.post('/getName', checkIfAdmin, async (req, res) => {
+  const firstName = req.body.user.split(" ")[0];
+  fetch(`https://azurenodefunc.azurewebsites.net/api/HelloWorldTrigger?name=${firstName}`)
+    .then(response => response.text())
+    .then(response => res.send({body : response}))
+});
+
+
 
 app.get(['/', '*'], function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
